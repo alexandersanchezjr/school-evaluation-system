c0aaf63d8e9278788a00e08978f8e0fe93794383
Alexander <alexssjr2002@gmail.com>
2021-05-26 23:14:39 -0500

    LoginGUI.java created

--- src/ui/LoginGUI.java
+++ src/ui/LoginGUI.java
@@ -1,4 +1,29 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginGUI {
+    @FXML
+    private AnchorPane mainWelcomePane;
+
+    @FXML
+    private TextField institutionalCodeTextField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    void logIn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerTeacher(ActionEvent event) {
+
+    }
+
 }
