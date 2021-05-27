8748aa879864e299215ce163043c5e42ef3471e7
Alexander <alexssjr2002@gmail.com>
2021-05-27 12:19:21 -0500

    ManagerGUI.java created

---
+++ src/ui/ManagerGUI.java
@@ -0,0 +1,40 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class ManagerGUI {
+
+    @FXML
+    private Label timeLabel;
+
+    @FXML
+    private AnchorPane mainPane;
+
+    @FXML
+    void logOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showAssessmentsPane(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showCoursesPane(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showExamsPane(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showQuestionnairesPane(ActionEvent event) {
+
+    }
+}
