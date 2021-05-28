808fe6a4e14b5d651ba3795cefac44b90a49dae6
Alexander <alexssjr2002@gmail.com>
2021-05-28 10:37:35 -0500

    RegisterGUI.java created

---
+++ src/ui/RegisterGUI.java
@@ -0,0 +1,46 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.GridPane;
+
+public class RegisterGUI {
+
+    @FXML
+    private GridPane mainRegisterGridPane;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private TextField surnameTextField;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private TextField codeTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private ToggleButton availabilityToogleButton;
+
+    @FXML
+    void changeAvailability(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void register(ActionEvent event) {
+
+    }
+}
