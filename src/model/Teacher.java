0091c1a453a090b8cbc558135b729a63ea0d5f42
santiagoarevalo <santiguitar2003@gmail.com>
2021-05-28 14:53:44 -0500

    Association with next Teacher implemented

--- src/model/Teacher.java
+++ src/model/Teacher.java
@@ -4,9 +4,25 @@ public class Teacher extends Member {
 
 	private boolean fullTime; //If this boolean value is true it means this teacher is full time, else he's half time teacher.
 	
+	private Teacher next;
+	
 	public Teacher(String name, String lastName, String email, String code, boolean fullTime) {
 		super(name, lastName, email, code);
 		this.fullTime = fullTime;
+	}
+
+	/**
+	 * @return the next Teacher
+	 */
+	public Teacher getNext() {
+		return next;
+	}
+
+	/**
+	 * @param next set the next Teacher
+	 */
+	public void setNext(Teacher next) {
+		this.next = next;
 	}
 
 	/**
