9856cbb502c563400f08ca349f1a0120a179cee9
santiagoarevalo <santiguitar2003@gmail.com>
2021-05-29 17:17:53 -0500

    Answers attribute added with his getter and setter and Constructor method modified

--- src/model/Workshop.java
+++ src/model/Workshop.java
@@ -1,10 +1,28 @@
 package model;
 
+import java.util.Calendar;
+
 public class Workshop extends Activity {
 
-	public Workshop(String topic, int percentage, String content) {
-		super(topic, percentage, content);
-		// TODO Auto-generated constructor stub
+	private String answers;
+	
+	public Workshop(String topic, int percentage, String content, Calendar date, String answers) {
+		super(topic, percentage, content, date);
+		this.setAnswers(answers);
+	}
+
+	/**
+	 * @return the answers
+	 */
+	public String getAnswers() {
+		return answers;
+	}
+
+	/**
+	 * @param answers the answers to set
+	 */
+	public void setAnswers(String answers) {
+		this.answers = answers;
 	}
 
 }
