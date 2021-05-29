50533f41e657331c2ef58fca7d14c0374ea9a9c9
santiagoarevalo <santiguitar2003@gmail.com>
2021-05-29 17:15:57 -0500

    timeLimit attribute added with his getters and setters

--- src/model/Exam.java
+++ src/model/Exam.java
@@ -2,9 +2,26 @@ package model;
 
 public class Exam extends Evaluation {
 
-	public Exam(String topic, int percentage, String content) {
+	private int timeLimit;
+	
+	public Exam(String topic, int percentage, String content, int timeLimit) {
 		super(topic, percentage, content);
+		this.setTimeLimit(timeLimit);
 		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the timeLimit
+	 */
+	public int getTimeLimit() {
+		return timeLimit;
+	}
+
+	/**
+	 * @param timeLimit the timeLimit to set
+	 */
+	public void setTimeLimit(int timeLimit) {
+		this.timeLimit = timeLimit;
 	}
 
 }
