fb753fd6f87d89d8063d47548e946d05f73fdb44
santiagoarevalo <santiguitar2003@gmail.com>
2021-05-29 17:15:08 -0500

    All management Student methods passed to the Course class

--- src/model/EvaluationSystem.java
+++ src/model/EvaluationSystem.java
@@ -1,6 +1,5 @@
 package model;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class EvaluationSystem {
@@ -107,49 +106,6 @@ public class EvaluationSystem {
 		return teacherSearched;
 	}
 	
-	//MANAGAMENT STUDENTS
 	
-	//Add Student
-	
-	public boolean addStudent(String name, String lastName, String email, String code) throws IOException {
-		boolean added = false;
-		Student newStudent = new Student(name, lastName, email, code);
-		if(!students.contains(newStudent)) {
-			added = students.add(newStudent);
-		}
-		return added;
-	}
-	
-	//Update Student info
-	
-	public void updateStudent(Student s, String name, String lastName, String email, String code) throws IOException {
-		int index = students.indexOf(s);
-		students.get(index).setName(name);
-		students.get(index).setLastName(lastName);
-		students.get(index).setEmail(email);
-		students.get(index).setCode(code);
-	}
-	
-	//Delete Student
-	
-	public boolean deleteStudent(Student s) throws IOException {
-		boolean deleted = false;
-		if(students.contains(s)) {
-			deleted = students.remove(s);
-		}
-		return deleted;
-	}
-	
-	//Search Student
-	
-	public Student searchStudent(String code) {
-		Student studentSearched = null;
-		for(int i = 0; i < students.size(); i++) {
-			if(students.get(i).getCode().equals(code)) {
-				studentSearched = students.get(i);
-			}
-		}
-		return studentSearched;
-	}
 	
 }
