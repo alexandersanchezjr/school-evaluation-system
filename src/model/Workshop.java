41579c9807662345624c73222c038ca0c45b8168
santiagoarevalo <santiguitar2003@gmail.com>
2021-05-28 23:48:05 -0500

    Workshop class created

---
+++ src/model/Workshop.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Workshop extends Activity {
+
+	public Workshop(String topic, int percentage, String content) {
+		super(topic, percentage, content);
+		// TODO Auto-generated constructor stub
+	}
+
+}
