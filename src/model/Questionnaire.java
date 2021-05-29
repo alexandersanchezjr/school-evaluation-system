d0258314b147361cd481670565f18eb000f9505e
santiagoarevalo <santiguitar2003@gmail.com>
2021-05-28 23:48:25 -0500

    Questionnaire class created

---
+++ src/model/Questionnaire.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Questionnaire extends Activity {
+
+	public Questionnaire(String topic, int percentage, String content) {
+		super(topic, percentage, content);
+		// TODO Auto-generated constructor stub
+	}
+
+}
