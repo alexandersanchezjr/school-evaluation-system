5b69803d2b835f9d0a81298e26c46b16019c8454
santiagoarevalo <santiguitar2003@gmail.com>
2021-05-28 23:47:24 -0500

    Exam class created

---
+++ src/model/Exam.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Exam extends Evaluation {
+
+	public Exam(String topic, int percentage, String content) {
+		super(topic, percentage, content);
+		// TODO Auto-generated constructor stub
+	}
+
+}
