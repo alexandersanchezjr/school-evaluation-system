60249ae611a8b638b7dad2b1ef66d786acb4779e
Alexander <alexssjr2002@gmail.com>
2021-05-30 21:47:50 -0500

    export and import buttons working

--- src/ui/ExamsGUI.java
+++ src/ui/ExamsGUI.java
@@ -2,29 +2,38 @@ package ui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import model.EvaluationSystem;
+import model.Exam;
+
+import java.io.File;
 
 public class ExamsGUI {
+
+    private EvaluationSystem evaluationSystem;
 
     @FXML
     private AnchorPane adminPane;
 
     @FXML
-    private TableView<?> examsTableView;
+    private TableView<Exam> examsTableView;
 
     @FXML
-    private TableColumn<?, ?> tcExamsName;
+    private TableColumn<Exam, String> tcExamsName;
 
     @FXML
-    private TableColumn<?, ?> tcExamsPercentage;
+    private TableColumn<Exam, String> tcExamsPercentage;
 
     @FXML
-    private TableColumn<?, ?> tcExamsFinalDate;
+    private TableColumn<Exam, String> tcExamsFinalDate;
 
     @FXML
-    private TableColumn<?, ?> tcExamsLimitTime;
+    private TableColumn<Exam, String> tcExamsLimitTime;
 
     @FXML
     private TextField examNameTextField;
@@ -47,6 +56,10 @@ public class ExamsGUI {
     @FXML
     private ChoiceBox<?> coursesChoiceBox;
 
+    public ExamsGUI(EvaluationSystem evaluationSystem) {
+        this.evaluationSystem = evaluationSystem;
+    }
+
     @FXML
     void cleanList(ActionEvent event) {
 
@@ -64,12 +77,35 @@ public class ExamsGUI {
 
     @FXML
     void exportExamsList(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
 
+        //Set extension filter for text files
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
+        fileChooser.getExtensionFilters().add(extFilter);
+        fileChooser.setInitialFileName("Reporte-Examenes.csv");
+
+        //Show save file dialog
+
+        File file = fileChooser.showSaveDialog((Stage)((Node)event.getSource()).getScene().getWindow());
+
+        if (file != null) {
+
+        }
     }
 
     @FXML
     void importExamsList(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
 
+        //Set extension filter for text files
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        File file = fileChooser.showOpenDialog((Stage)((Node)event.getSource()).getScene().getWindow());
+
+        if (file != null) {
+
+        }
     }
 
     @FXML
