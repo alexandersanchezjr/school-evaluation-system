aa134554abe61aae015ff42795c273469791304d
Alexander <alexssjr2002@gmail.com>
2021-06-10 10:17:00 -0500

    methods finished

--- src/ui/CoursesGUI.java
+++ src/ui/CoursesGUI.java
@@ -1,38 +1,44 @@
 package ui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import model.Course;
-import model.EvaluationSystem;
+import model.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Optional;
 
 public class CoursesGUI {
 
 
     private EvaluationSystem evaluationSystem;
+    private Course currentCourse;
 
     @FXML
-    private TableView<Course> studentsTableView;
+    private TableView<Student> studentsTableView;
 
     @FXML
-    private TableColumn<Course, String> tcStudentName;
+    private TableColumn<Student, String> tcStudentName;
 
     @FXML
-    private TableColumn<Course, String> tcStudentSurname;
+    private TableColumn<Student, String> tcStudentSurname;
 
     @FXML
-    private TableColumn<Course, String> tcStudentCode;
+    private TableColumn<Student, String> tcStudentCode;
 
     @FXML
-    private TableColumn<Course, String> tcStudentEmail;
+    private TableColumn<Student, String> tcStudentEmail;
 
     @FXML
     private TextField studentNameTextField;
@@ -80,33 +86,112 @@ public class CoursesGUI {
         this.evaluationSystem = evaluationSystem;
     }
 
-    private void initializeCoursesTableView () {
-
-    }
-
     @FXML
     void changeCourseName(InputMethodEvent event) {
-
+        currentCourse.setCourseName(courseNameLabel.getText());
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Operación Exitosa");
+        alert.setHeaderText(null);
+        alert.setContentText("El nombre del curso fue cambiado exitosamente.");
+        alert.showAndWait();
     }
 
-    @FXML
+/*    @FXML
     void chooseNewPhoto(ActionEvent event) {
 
-    }
+    }*/
 
     @FXML
     void cleanList(ActionEvent event) {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Confirme borrado de datos");
+        dialog.setHeaderText("Por favor, escriba 'SI' si desea limpiar la lista de estudiantes ");
 
+        // Traditional way to get the response value.
+        String input = "";
+        Optional<String> result = dialog.showAndWait();
+        if (result.isPresent()){
+            input = result.get();
+        }
+        if (input.equals("SI")) {
+            currentCourse.getStudents().clear();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Operación exitosa");
+            alert.setHeaderText("Lista de estudiantes eliminada");
+            alert.setContentText("Todos los estudiantes han sido borrados existosamente. Presione OK");
+            alert.showAndWait();
+
+            initializeStudentTableView();
+            studentNameTextField.setDisable(true);
+            studentNameTextField.setText(null);
+
+            studentSurnameTextField.setDisable(true);
+            studentSurnameTextField.setText(null);
+
+            studentCodeTextField.setDisable(true);
+            studentCodeTextField.setText(null);
+
+            studentEmailTextField.setDisable(true);
+            studentEmailTextField.setText(null);
+
+            studentGradeTextField.setDisable(true);
+            studentGradeTextField.setText(null);
+        }
     }
 
     @FXML
     void createNewStudent(ActionEvent event) {
+        if(newStudentHasEmptyField()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Campos Vacíos");
+            alert.setHeaderText(null);
+            alert.setContentText("Por favor, rellene todos los campos para poder crear un estudiante");
+            alert.showAndWait();
+        }else {
+            String name = newStudentName.getText();
+            String lastname = newStudentSurname.getText();
+            String code = newStudentCode.getText();
+            String email = newStudentEmail.getText();
 
+            try {
+                if (currentCourse.addStudent(name, lastname, email, code)){
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Operación exitosa");
+                    alert.setHeaderText("Estudiante creado");
+                    alert.setContentText("El estudiante ha sido creado existosamente. Presione OK");
+                    alert.showAndWait();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
+    private boolean newStudentHasEmptyField() {
+        boolean emptyField = false;
+        if (newStudentName.getText().isEmpty())
+            emptyField = true;
+        if (newStudentSurname.getText().isEmpty())
+            emptyField = true;
+        if (newStudentCode.getText().isEmpty())
+            emptyField = true;
+        if (newStudentEmail.getText().isEmpty())
+            emptyField = true;
+
+        return emptyField;
+    }
+
 
     @FXML
     void deleteStudent(ActionEvent event) {
-
+        Student student = studentsTableView.getSelectionModel().getSelectedItem();
+        if (currentCourse.getStudents().remove(student)){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Operación exitosa");
+            alert.setHeaderText("Estudiante eliminado");
+            alert.setContentText("El estudiante ha sido eliminado existosamente. Presione OK");
+            alert.showAndWait();
+        }
     }
 
     @FXML
@@ -123,7 +208,12 @@ public class CoursesGUI {
         File file = fileChooser.showSaveDialog((Stage)((Node)event.getSource()).getScene().getWindow());
 
         if (file != null) {
-
+            String separator = ";";
+            try {
+                currentCourse.exportStudents(file.getAbsolutePath(), separator);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -138,31 +228,112 @@ public class CoursesGUI {
         File file = fileChooser.showOpenDialog((Stage)((Node)event.getSource()).getScene().getWindow());
 
         if (file != null) {
-
+            String separator = ";";
+            try {
+                currentCourse.importStudents(file.getAbsolutePath(), separator);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     @FXML
     void showStudentInfo(MouseEvent event) {
+        Student student = studentsTableView.getSelectionModel().getSelectedItem();
+        if (student != null) {
+            studentNameTextField.setDisable(false);
+            studentNameTextField.setText(student.getName());
 
+            studentSurnameTextField.setDisable(false);
+            studentSurnameTextField.setText(student.getLastName());
+
+            studentCodeTextField.setDisable(false);
+            studentCodeTextField.setText(student.getCode());
+
+            studentEmailTextField.setDisable(false);
+            studentEmailTextField.setText(student.getEmail());
+
+            studentGradeTextField.setDisable(false);
+            studentGradeTextField.setText(student.getFinalAverageGrade() + "");
+        }else {
+            studentNameTextField.setDisable(true);      //Deactivate student name text field
+            studentNameTextField.setText(null);
+
+            studentSurnameTextField.setDisable(true);   //Deactivate student lastname text field
+            studentSurnameTextField.setText(null);
+
+            studentCodeTextField.setDisable(true);      //Deactivate student code text field
+            studentCodeTextField.setText(null);
+
+            studentEmailTextField.setDisable(true);     //Deactivate student email text field
+            studentEmailTextField.setText(null);
+
+            studentGradeTextField.setDisable(true);     //Deactivate student grade text field
+            studentGradeTextField.setText(null);
+        }
     }
 
     @FXML
     void updateStudent(ActionEvent event) {
+        Student student = studentsTableView.getSelectionModel().getSelectedItem();
 
+        if (!isEmptyField()){
+            String name = studentNameTextField.getText();
+            String lastname = studentSurnameTextField.getText();
+            String code = studentCodeTextField.getText();
+            String email = studentEmailTextField.getText();
+            try {
+                currentCourse.updateStudent(student, name, lastname, email, code);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Campos Vacíos");
+            alert.setHeaderText(null);
+            alert.setContentText("Por favor, rellene todos los campos para poder actualizar un estudiante");
+            alert.showAndWait();
+        }
     }
 
-    @FXML
+    private boolean isEmptyField() {
+        boolean emptyField = false;
+        if (studentNameTextField.getText().isEmpty()){
+            emptyField = true;
+        }
+        if (studentSurnameTextField.getText().isEmpty()){
+            emptyField = true;
+        }
+        if (studentCodeTextField.getText().isEmpty()){
+            emptyField = true;
+        }
+        if (studentEmailTextField.getText().isEmpty()){
+            emptyField = true;
+        }
+        return emptyField;
+    }
+
+/*    @FXML
     void updateStudentPhoto(ActionEvent event) {
 
-    }
+    }*/
 
     public void initialize(Course selectedCourse) {
-        courseNameLabel.setText(selectedCourse.getCourseName());
-        initializeStudentTableView(selectedCourse);
+        currentCourse = selectedCourse;
+        courseNameLabel.setText(currentCourse.getCourseName());
+        initializeStudentTableView();
     }
 
-    private void initializeStudentTableView(Course selectedCourse) {
-        //Todo fill the student table view
+    private void initializeStudentTableView() {
+        ObservableList<Student> list = FXCollections.observableArrayList(currentCourse.getStudents());
+
+        studentsTableView.setItems(list);
+        tcStudentName.setCellValueFactory(new PropertyValueFactory<Student, String>("name"));
+        tcStudentSurname.setCellValueFactory(new PropertyValueFactory<Student, String>("lastName"));
+        tcStudentCode.setCellValueFactory(new PropertyValueFactory<Student, String>("code"));
+        tcStudentEmail.setCellValueFactory(new PropertyValueFactory<Student, String>("email"));
+
+        studentsTableView.refresh();
+
     }
 }
