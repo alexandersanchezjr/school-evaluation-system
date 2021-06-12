a2b996b26caa14935f6c0a0efd212e4ba23322ca
santiagoarevalo <santiguitar2003@gmail.com>
2021-06-12 15:15:06 -0500

    serialVersionUID added

--- src/exceptions/EmptyEvaluationException.java
+++ src/exceptions/EmptyEvaluationException.java
@@ -1,7 +1,11 @@
 package exceptions;
 
-@SuppressWarnings("serial")
 public class EmptyEvaluationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public EmptyEvaluationException() {
 		super("La evaluacion no tiene contenido");
