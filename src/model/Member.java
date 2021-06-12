77dcd924f313d95465ec05683df5c4323d332aba
Alexander <alexssjr2002@gmail.com>
2021-06-12 09:26:35 -0500

    implements Serializable

--- src/model/Member.java
+++ src/model/Member.java
@@ -1,6 +1,8 @@
 package model;
 
-public abstract class Member {
+import java.io.Serializable;
+
+public abstract class Member implements Serializable {
 	private String name;
 	private String lastName;
 	private String email;
